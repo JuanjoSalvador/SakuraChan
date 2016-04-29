1970c0d162cf10b2258d691a8f71388d3b5e7d72
Juanjo Salvador <juanjosalvador@openmailbox.org>
2016-04-29 18:23:59 +0200

    Añadido comando LIKE y reparado el envío de traducciones

--- SakuraChan.js
+++ SakuraChan.js
@@ -1,38 +1,47 @@
 // Requires
-var TelegramBot = require('node-telegram-bot-api');
-var bing        = require('bing-translate');                    
+var TelegramBot = require('node-telegram-bot-api');            
 
 // Token
 var token = ''; // Cambiar el token por el token de SakuraTranslate
-bing.init({ 
-        client_id: 'SakuraTranslate_bot', 
-        client_secret: '' // Token secreto de Sakura en Bing Translate
-       });
+var bt = require('bing-translate').init({ 
+                                        client_id: 'SakuraTranslate_bot', 
+                                        client_secret: '' // Token secreto de Sakura en Bing Translate
+                                      });
 
 // Inicialización
 var sakura = new TelegramBot(token, {polling: true});
 console.log("Sakura-chan está funcionando!");
-var jap2esp = 0; // Contador de traducciones japones-español
-var esp2jap = 0; // Contador de traducciones español-japones
+
+// Start
+sakura.onText(/\/start/, function (msg) {
+  var fromId = msg.from.id;
+  sakura.sendMessage(fromId, "Konnichiwa!");
+});
 
 // Japonés a Español
 sakura.onText(/\/jp (.+)/, function (msg, match) {
+  console.log("Mensaje enviado: ", msg.text); // Debug
   var fromId = msg.from.id;
   var input  = match[1];
-  bing.translate(input, 'ja', 'es', function(err, res) { sakura.sendMessage(fromId, "La traducción para '" + input + "' es: " + res.translated_text) });
-  jap2esp++;
+  bt.translate(input, 'ja', 'es', function(err, res) { sakura.sendMessage(fromId, "'" + input + "', traducido, significa '" + res.translated_text + "'."); });
 });
 
 // Español a Japonés
 sakura.onText(/\/esp (.+)/, function (msg, match) {
+  console.log("Mensaje enviado: ", msg.text); // Debug
   var fromId = msg.from.id;
   var input  = match[1];
-  bing.translate(input, 'es', 'ja', function(err, res) { sakura.sendMessage(fromId, "La traducción para '" + input + "' es: " + res.translated_text) });
-  esp2jap++;
+  bt.translate(input, 'es', 'ja', function(err, res) { sakura.sendMessage(fromId, "En japonés, '" + input + "' se escribe " + res.translated_text) });
 });
 
 // Repo
 sakura.onText(/\/repo/, function (msg) {
   var fromId = msg.from.id;
   sakura.sendMessage(fromId, "https://github.com/JuanjoSalvador/SakuraChan");
+});
+
+// Like
+sakura.onText(/\/like/, function (msg) {
+  var fromId = msg.from.id;
+  sakura.sendMessage(fromId, "¡Gracias " + msg.from.first_name + "! ¡A Sakura-chan también le gustas tu!");
 });
